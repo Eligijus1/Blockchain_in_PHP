3c7abb7e174af3d1e5d3ce1c62a6f702826813f8
Eligijus Stugys <eligijus.stugys@nfq.lt>
2018-05-10 14:46:16 +0300

    Fixing index.

--- html/Service/BlockManager.php
+++ html/Service/BlockManager.php
@@ -15,7 +15,7 @@ class BlockManager
 {
     public function calculateHash(Block $block): string {
         $record = (string)$block->index . $block->timestamp . (string)$block->bpm . $block->prevHash;
-        $hashed = hash('sha256', $_POST['ppasscode']);
+        $hashed = hash('sha256', $record);
         return $hashed;
     }
     
@@ -45,5 +45,18 @@ class BlockManager
 	    }
 
 	    return true;
+    }
+    
+    public function getGenesisBlock(): Block
+    {
+        $genesisBlock = new Block();
+        
+	    $genesisBlock->index = 0;
+	    $genesisBlock->timestamp = (string)time();
+        $genesisBlock->bpm = 0;
+        $genesisBlock->prevHash = "";
+	    $genesisBlock->hash = $this->calculateHash($genesisBlock);
+        
+        return $genesisBlock;
     }
 }
--- html/index.php
+++ html/index.php
@@ -5,10 +5,37 @@ include_once 'Service/BlockManager.php';
 use Blockchain\Model\Block;
 use Blockchain\Service\BlockManager;
 
-echo "\nBegin";
-
+// Get important variables:
+$dataFilePath = '';
+$action       = $_POST["action"];
+$bpm          = $_POST["bpm"];
 $blockManager = new BlockManager();
-$block = new Block();
-// $block->
+$block        = new Block();
+$blockChain   = [$blockManager->getGenesisBlock()];//unserialize( file_get_contents( $filePath ) );
 
-echo "\nEnd\n";
+// Check if "action" defined:
+if (empty($action)) {
+    error_log("'action' not defined.");
+    return;
+}
+
+// Check if valid action:
+if ($action !== 'view' && $action !== 'write') {
+    error_log("Unhandled action '{$action}' detected.");
+    return;
+}
+
+// Check if "bpm" is numeric:
+if (!is_numeric($bpm)) {
+    error_log("bpm value '{$bpm}' is not numeric.");
+    return;
+}
+
+// Print variables (DEBUG):
+echo "\naction={$action}";
+echo "\nbpm={$bpm}";
+echo "\n" . serialize($blockChain);
+echo "\n";
+
+// 
+file_put_contents($dataFilePath, serialize($blockChain));
